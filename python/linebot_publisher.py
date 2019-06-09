028e6e7c635d13dec41f966b085d3adcd9381408
Tsubasa Ogawa <hurikake@gmail.com>
2019-06-09 03:12:42 +0000

    APInized

--- python/linebot_publisher.py
+++ python/linebot_publisher.py
@@ -1,16 +1,61 @@
 import json
 import os
 import requests
+import sys
 
 
 class LineBotPublisher:
-  ENDPOINT = os.environ['LINE_BOT_ENDPOINT'] # 'https://api.line.me/v2/bot/message/reply'
-  ACCESS_TOKEN = os.environ['LINE_BOT_ACCESS_TOKEN']
+  """
+  LINEBot Publisher.
+  """
+  # URL Endpoint
+  ENDPOINT = 'https://api.line.me/v2/bot/message/push'
 
-  def post_text(self, to_id, text):
+  # Access Token
+  ACCESS_TOKEN = os.environ.get('LINE_BOT_ACCESS_TOKEN')
+
+  def __init__(self, endpoint=ENDPOINT, access_token=ACCESS_TOKEN):
+    """
+    Constructor.
+
+    Parameters
+    ----------
+    endpoint : string
+        URL Endpoint to LINE Messaging API.
+        Default is https://api.line.me/v2/bot/message/push
+
+    access_token : string
+        Access token of LINE Messaging API.
+    """
+    self._die_if_empty([endpoint, access_token])
+
+    self.endpoint = endpoint
+    self.access_token = access_token
+
+  def post_text(self, to_id, text, notifies=True):
+    """
+    Post text to LINE Bot.
+
+    Parameters
+    ----------
+    to_id : string
+        userId, groupId or roomId
+    text : string
+        Message text
+    notifies : bool
+        Notify user(s) when it posts if True.
+        Default is True.
+
+    Returns
+    -------
+    response : Response
+        It always returns 200.
+    """
+    self._die_if_empty([to_id, text])
+
     header = {
       "Content-Type": "application/json",
-      "Authorization": "Bearer {0}".format(self.ACCESS_TOKEN)
+      "Authorization": "Bearer {}".format(self.access_token)
     }
     payload = {
       "to": to_id,
@@ -19,9 +64,29 @@ class LineBotPublisher:
           "type": "text",
           "text": text
         }
-      ]
+      ],
+      "notificationDisabled": not notifies
     }
-    requests.post(self.ENDPOINT, headers=header, data=json.dumps(payload))
 
-    return True
+    response = requests.post(
+        self.endpoint,
+        headers=header,
+        data=json.dumps(payload)
+    )
+
+    return response
+
+  def _die_if_empty(self, objects=[]):
+    """
+    Print message and exit program as an error if a given element is empty ('').
+
+    Parameters
+    ----------
+    objects : list[string]
+        List of string objects.
+    """
+    for obj in objects:
+      if obj == '':
+        print('{} is null.'.format(obj))
+        sys.exit(1)
 
